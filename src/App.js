064b7d1868b9df2321259927ca90d19299c61376
nicholas570 <nicolas.mercier570@gmail.com>
2020-10-28 10:31:39 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+import './App.css';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.mapRef = React.createRef();
+    this.state = {
+      lat: 47.232964,
+      lng: -1.51134,
+      zoom: 13,
+    };
+    this.handleOnLocationFound = this.handleOnLocationFound.bind(this);
+    this.handleOnLocationError = this.handleOnLocationError.bind(this);
+  }
+
+  componentDidMount() {
+    const { current } = this.mapRef;
+    const { leafletElement: map } = current;
+    map.locate({ setView: true });
+    map.on('locationfound', this.handleOnLocationFound);
+    map.on('locationerror', this.handleOnLocationError);
+  }
+
+  handleOnLocationFound(e) {
+    const { current } = this.mapRef;
+    const { leafletElement: map } = current;
+
+    const latlng = e.latlng;
+    const marker = L.marker(latlng);
+    marker.addTo(map).bindPopup('Votre position ').openPopup();
+  }
+
+  handleOnLocationError(e) {
+    const { current } = this.mapRef;
+    const { leafletElement: map } = current;
+    map.locate({ setView: false });
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map ref={this.mapRef} center={position} zoom={this.state.zoom}>
+        <TileLayer
+          url='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
 
 export default App;
